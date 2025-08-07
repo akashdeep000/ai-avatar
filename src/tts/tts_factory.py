1f612aae81189eb897c77522697ca551d0d727e0
AkashDeep000 <akashdeepdas970@gmail.com>
2025-08-07 11:50:41 +0530

    added bark_tts and fixed pip install

--- src/tts/tts_factory.py
+++ src/tts/tts_factory.py
@@ -2,6 +2,7 @@ from .tts_interface import TTSInterface
 from .dummy_tts import DummyTTS
 from .edge_tts import EdgeTTS
 from .chatterbox_tts import ChatterboxTTS
+from .bark_tts import BarkTTS
 
 class TTSFactory:
     @staticmethod
@@ -11,7 +12,9 @@ class TTSFactory:
         elif engine_name == "edge_tts":
             voice = kwargs.pop('voice', 'en-US-AvaMultilingualNeural')
             return EdgeTTS(voice=voice, **kwargs)
-        elif engine_name == "chatterbox":
+        elif engine_name == "chatterbox_tts":
             return ChatterboxTTS(**kwargs)
+        elif engine_name == "bark_tts":
+            return BarkTTS(**kwargs)
         else:
             raise ValueError(f"Unknown TTS engine: {engine_name}")
